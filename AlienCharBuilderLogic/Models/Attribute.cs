00689713917e9b41b514a6760e512b3d8677c26f
Assma-Fl <f-assmann@gmx.de>
2024-02-29 13:10:04 +0100

    Min/Max für Fertigkeiten und Attribute

--- AlienCharBuilderLogic/Models/Attribute.cs
+++ AlienCharBuilderLogic/Models/Attribute.cs
@@ -12,7 +12,7 @@ namespace AlienCharBuilderLogic.Models
 
         [ComplexDataAttribute]
         public Wits Wits { get; set; } = new Wits();
-
+        
         [ComplexDataAttribute]
         public Empathy Empathy { get; set; } = new Empathy();
     }
@@ -20,12 +20,14 @@ namespace AlienCharBuilderLogic.Models
     public class Strength
     {
         [SheetnameAttribute(Sheetname = Constants.STRENGTH)]
+        [MinMaxAttribute(Min = 2, Max = 4)]
         public int Value { get; set; }
 
         private int _closeCombat;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.CLOSE_COMBAT)]
-        public int CloseCombat 
-        { 
+        public int CloseCombat
+        {
             get
             {
                 return Value + _closeCombat;
@@ -37,9 +39,10 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _stamina;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.STAMINA)]
-        public int Stamina 
-        { 
+        public int Stamina
+        {
             get
             {
                 return Value + _stamina;
@@ -51,8 +54,9 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _heavyMachinery;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.HEAVY_MACHINERY)]
-        public int HeavyMachinery 
+        public int HeavyMachinery
         {
             get
             {
@@ -67,13 +71,15 @@ namespace AlienCharBuilderLogic.Models
 
     public class Agility
     {
-        [SheetnameAttribute(Sheetname = Constants.AGILITY)]
+        [MinMaxAttribute(Min = 2, Max = 4)]
+        [SheetnameAttribute(Sheetname = Constants.AGILITY)]        
         public int Value { get; set; }
 
         private int _piloting;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.PILOTING)]
-        public int Piloting 
-        { 
+        public int Piloting
+        {
             get
             {
                 return Value + _piloting;
@@ -85,8 +91,9 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _mobilty;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.MOBILTY)]
-        public int Mobilty 
+        public int Mobilty
         {
             get
             {
@@ -99,8 +106,9 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _rangedCombat;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.RANGED_COMBAT)]
-        public int RangedCombat 
+        public int RangedCombat
         {
             get
             {
@@ -115,13 +123,15 @@ namespace AlienCharBuilderLogic.Models
 
     public class Empathy
     {
+        [MinMaxAttribute(Min = 2, Max = 4)]
         [SheetnameAttribute(Sheetname = Constants.EMPATHY)]
         public int Value { get; set; }
 
         private int _medicalAid;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.MEDICALAID)]
-        public int MedicalAid 
-        { 
+        public int MedicalAid
+        {
             get
             {
                 return Value + _medicalAid;
@@ -133,9 +143,10 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _manipulation;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.MANIPULATION)]
-        public int Manipulation 
-        { 
+        public int Manipulation
+        {
             get
             {
                 return Value + _manipulation;
@@ -147,15 +158,16 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _command;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.COMMAND)]
-        public int Command 
-        { 
+        public int Command
+        {
             get
-            {
+            {                
                 return Value + _command;
             }
             set
-            {
+            {                
                 _command = value;
             }
         }
@@ -164,13 +176,15 @@ namespace AlienCharBuilderLogic.Models
 
     public class Wits
     {
+        [MinMaxAttribute(Min = 2, Max = 4)]
         [SheetnameAttribute(Sheetname = Constants.WITS)]
         public int Value { get; set; }
 
         private int _observation;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.OBSERVATION)]
-        public int Observation 
-        { 
+        public int Observation
+        {
             get
             {
                 return Value + _observation;
@@ -181,10 +195,11 @@ namespace AlienCharBuilderLogic.Models
             }
         }
 
-        private int _survival; 
+        private int _survival;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.SURVIVAL)]
-        public int Survival 
-        { 
+        public int Survival
+        {
             get
             {
                 return Value + _survival;
@@ -196,9 +211,10 @@ namespace AlienCharBuilderLogic.Models
         }
 
         private int _comtech;
+        [MinMaxAttribute(Min = 0, Max = 5)]
         [SheetnameAttribute(Sheetname = Constants.COMTECH)]
-        public int Comtech 
-        { 
+        public int Comtech
+        {
             get
             {
                 return Value + _comtech;
@@ -208,8 +224,7 @@ namespace AlienCharBuilderLogic.Models
                 _comtech = Value;
             }
         }
-
     }
 
-   
+
 }
